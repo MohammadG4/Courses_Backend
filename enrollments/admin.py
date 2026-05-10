5355749aa2a5eda8de55cfcf463dc72d3b436303
MohammadG4 <99698684+MohammadG4@users.noreply.github.com>
2026-05-10 12:06:55 +0300

    Add Enrollment model and initial migration; update admin registration

--- enrollments/admin.py
+++ enrollments/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from enrollments.models import Enrollment
+admin.site.register(Enrollment)
+
 # Register your models here.
--- finance/admin.py
+++ finance/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import PlatformTier, Purchase, InstructorRevenue
+
+admin.site.register(PlatformTier)
+admin.site.register(Purchase)
+admin.site.register(InstructorRevenue)
+
 # Register your models here.
